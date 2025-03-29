25fbeefa0221275689fd14925151c199554409ca
Emmy <158222177+hmEmmy@users.noreply.github.com>
2025-03-29 09:04:34 +0100

    Initial commit

---
+++ src/main/java/me/emmy/core/Flash.java
@@ -0,0 +1,17 @@
+package me.emmy.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Flash extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
